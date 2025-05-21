d8c33c6a655f423e322cc53e28791ad194595dc3
jakub-ozog <jakubjanozog@gmail.com>
2025-05-21 23:51:11 +0200

    feat: add JWT config

--- dist/config/plugins.js
+++ dist/config/plugins.js
@@ -1,3 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = () => ({});
+exports.default = ({ env }) => ({
+    'users-permissions': {
+        config: {
+            jwtSecret: env('JWT_SECRET'),
+        },
+    },
+});
